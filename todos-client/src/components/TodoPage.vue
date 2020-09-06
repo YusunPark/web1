cd817163867eb9f1efd132997f030eb252226d21
Yusun <solarpy@hanyang.ac.kr>
2020-09-06 22:49:12 +0900

    [삭제] localStorage 적용 삭제

--- todos-client/src/components/TodoPage.vue
+++ todos-client/src/components/TodoPage.vue
@@ -8,16 +8,16 @@
         class="form-control"
         placeholder="할일을 입력하세요"
         v-model="name"
-        v-on:keyup.enter="createTodo(name, index)"
+        v-on:keyup.enter="createTodo(name)"
       />
       <span class="input-group-btn">
-        <button class="btn btn-default" type="button" @click="createTodo(name, index)">Add</button>
+        <button class="btn btn-default" type="button" @click="createTodo(name)">Add</button>
       </span>
     </div>
     <ul class="list-group">
       <!-- 여러개를 선택하는 거니까 v-for  -->
-      <li class="list-group-item" v-for="(todo, index) in todos" :key="index">
-        {{ todo }}
+      <li class="list-group-item" v-for="(todo, index) in todos" :key="index" :class={status}>
+        {{ todo.name }}
         <div class="btn-group pull-right" style="font-size:12px; line-height: 1;">
           <button
             type="button"
@@ -31,7 +31,7 @@
           </button>
           <ul class="dropdown-menu">
             <li>
-              <a href="#" @click="doing(todo, index)">🏃‍♀️ Doing</a>
+              <a href="#" @click="doing(todo)">🏃‍♀️ Doing</a>
             </li>
             <li>
               <a href="#" @click="done(todo)">🤸‍♀️ Done</a>
@@ -65,7 +65,7 @@ export default {
     return {
       index: 0,
       name: "",
-      change_name: null,
+      change_name: "",
       todos: [],
     };
   },
@@ -76,51 +76,59 @@ export default {
   //   localStorage.setItem("item1", "퇴근");
   //   this.item1 = localStorage.getItem("item1");
   // },
-  mounted() {
-    let len = localStorage.length;
-    this.index = len;
-    for (let i = 0; i < len - 1; i++) {
-      let todo = localStorage.getItem(`todo${i}`);
-      this.todos.unshift(todo);
-    }
-  },
-  watch: {
-    index() {
-      this.todos = [];
-      let len = localStorage.length;
-      for (let i = 0; i < len - 1; i++) {
-        let todo = localStorage.getItem(`todo${i}`);
-        this.todos.unshift(todo);
+  
+  // localStorage 잠시 제거 
+  // mounted() {
+  //   let len = localStorage.length;
+  //   this.index = len;
+  //   for (let i = 0; i < len - 1; i++) {
+  //     let todo = localStorage.getItem(`todo${i}`);
+  //     this.todos.unshift(todo);
+  //   }
+  // },
+  // watch: {
+  //   index() {
+  //     this.todos = [];
+  //     let len = localStorage.length;
+  //     for (let i = 0; i < len - 1; i++) {
+  //       let todo = localStorage.getItem(`todo${i}`);
+  //       this.todos.unshift(todo);
+  //     }
+  //   },
+  // },
+  methods: {
+    createTodo(name, index) {
+      if (name != null) {
+        // 맨앞에 todo 추가
+        // let todo = name;
+        // localStorage.setItem(`todo${index}`, todo);
+        this.todos.unshift({ name: name, status: "todo" });
+
+        this.name = null;
+        this.index += 1;
       }
     },
-  },
-  methods: {
-    doing(todo, i) {
-      // this.todos.splice(i, 0, { status: "doing" });
-      todo.status = "doing";
+    deleteTodo(i) {
+      // i 인덱스 부터 1개를 삭제한다.
+      this.todos.splice(i, 1);
     },
-    done(i) {},
     editTodo(todo, change_name) {
       if (change_name != null) {
         todo.name = change_name;
       }
       this.change_name = null;
     },
-    deleteTodo(i) {
-      // i 인덱스 부터 1개를 삭제한다.
-      this.todos.splice(i, 1);
-    },
-    createTodo(name, index) {
-      if (name != null) {
-        // 맨앞에 todo 추가
-        let todo = name;
-        localStorage.setItem(`todo${index}`, todo);
-        // this.todos.unshift({ name: name, status: "todo" });
-
-        this.name = null;
-        this.index += 1;
+    doing(todo) {
+      if(todo.status == "doing"){
+        todo.status = "todo"
+      }
+      else{
+      todo.status = "doing";
       }
     },
+    done(i) {},
+    
+    
     All() {
       localStorage.clear();
       this.index = 0;
